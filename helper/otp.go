4263c686c08daf5813adde09808875810b753aef
Athul <akkv.2507@gmail.com>
2024-10-13 19:56:13 +0530

    Written OTP Function

--- helper/otp.go
+++ helper/otp.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"net/smtp"
 	"os"
 )
 
@@ -24,5 +25,17 @@ func SendEmail(to, otp string) {
 	from := os.Getenv("EMAIL_ADDRESS")
 	password := os.Getenv("EMAIL_PASSWORD")
 
-	if from =="" || password
+	if from == "" || password == "" {
+		log.Fatal("Email credentials not set in environment variable")
+	}
+
+	msg := "Your OTP for Signup is" + otp
+
+	err := smtp.SendMail("smtp.example.com:587", smtp.PlainAuth("", from, password, "smtp.example.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Fatal("Error Sending otp", err)
+	}
+	fmt.Println("OTP sent successfully")
 }
